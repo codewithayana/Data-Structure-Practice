18b60c9fd3519c53ece5c041ba78df6c0469531e
codewithayana <codewithayana@gmail.com>
2025-11-10 11:22:34 +0530

    added basics

---
+++ Arrays/basic.js
@@ -0,0 +1,25 @@
+/* Traversal  */
+// let numbers = [10, 20, 30, 40, 50];
+
+// For loop
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// For...of loop
+// for (let num of numbers) {
+//   console.log(num);
+// }
+
+// ForEach
+// numbers.forEach(num => console.log(num));
+
+/* Declaration */
+// Using square brackets 
+// let arr1 = [5,6,7];
+
+// using Array constructor
+// let arr2 = new array(5,6,7);
+
+// console.log(arr1);
+// consloe.log(arr2);
